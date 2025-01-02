b0872b6016a7bbb8d714dac2bab2baa4b0943b3e
andrio <andrio1hanania@gmail.com>
2025-01-02 15:36:50 +0200

    Add PWA support

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import 'animate.css';
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
+import { registerSW } from 'virtual:pwa-register'
+
+if ('serviceWorker' in navigator) {
+    registerSW({
+        onNeedRefresh() {
+            console.log('New content available, click on reload button to update.')
+        },
+        onOfflineReady() {
+            console.log('App ready to work offline')
+        },
+    })
+}
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,62 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    VitePWA({ 
+      registerType: 'autoUpdate',
+
+      includeAssets: ['favicon.ico'],
+
+      manifest: {
+        name: 'Miss Toy',
+        short_name: 'MissToy',
+        description: 'Digital Toys Shop',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: "standalone",
+        // scope: "/miss-toy/",
+        start_url: "/miss-toy/#/",
+        icons: [
+          {
+            src: "/miss-toy/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          ,
+          {
+            src: "/miss-toy/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          }
+        ],
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg}'],
+
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/andriohanania\.github\.io\/miss-toy\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'miss-toy-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 86400, // 1 day
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  
   base: '/miss-toy'
 })
